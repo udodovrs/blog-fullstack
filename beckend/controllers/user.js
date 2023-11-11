e6653efaf39995dfc29968ec016adb45adb73fd2
udodovrs <udodovrs@gmail.com>
2023-11-11 15:11:31 +0300

    add docker

--- beckend/controllers/user.js
+++ beckend/controllers/user.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const User = require("..\\models\\User");
+const User = require("../models/User");
 const { generate } = require("../helpers/token");
 const ROLES = require('../constants/roles')
 
